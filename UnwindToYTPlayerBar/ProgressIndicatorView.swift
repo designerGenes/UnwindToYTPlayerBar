cc07c5dab6effd636beb1dc591c7b50bd31b07fe
Jaden Nation <jnation@0503L.local>
2016-06-24 12:36:16 -0700

    about to switch to generic View

---
+++ UnwindToYTPlayerBar/ProgressIndicatorView.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressIndicatorView.swift
+//  UnwindToYTPlayerBar
+//
+//  Created by Jaden Nation on 6/24/16.
+//  Copyright © 2016 Jaden Nation. All rights reserved.
+//
+
+import UIKit
+
+class ProgressIndicatorView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
